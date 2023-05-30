319c5f56ce315f1e85f6e1dc88330e2b234c913d
Ralfi Ibaj <113370594+ribeyestk@users.noreply.github.com>
2023-05-30 13:51:43 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -2,12 +2,15 @@
 if(!file.exists("./Final Project")){dir.create("./Final Project")}
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(fileUrl,destfile="./Final Project/Dataset.zip",method="curl")
+
 ### Unzip the File
 unzip(zipfile="./Final Project/Dataset.zip",exdir="./Final Project")
+
 ### UCI HAR Data set has the unzipped files so Im extracting the list of them
 path_rf <- file.path("./Final Project" , "UCI HAR Dataset")
 files<-list.files(path_rf, recursive=TRUE)
 files
+
 ### Read the files
 dataActivityTest  <- read.table(file.path(path_rf, "test" , "Y_test.txt" ),header = FALSE)
 dataActivityTrain <- read.table(file.path(path_rf, "train", "Y_train.txt"),header = FALSE)
@@ -15,6 +18,7 @@ dataSubjectTrain <- read.table(file.path(path_rf, "train", "subject_train.txt"),
 dataSubjectTest  <- read.table(file.path(path_rf, "test" , "subject_test.txt"),header = FALSE)
 dataFeaturesTest  <- read.table(file.path(path_rf, "test" , "X_test.txt" ),header = FALSE)
 dataFeaturesTrain <- read.table(file.path(path_rf, "train", "X_train.txt"),header = FALSE)
+
 ### Observing properties
 str(dataActivityTest)
 str(dataActivityTrain)
@@ -22,6 +26,7 @@ str(dataSubjectTrain)
 str(dataSubjectTest)
 str(dataFeaturesTest)
 str(dataFeaturesTrain)
+
 ###Mergeing the training and the test sets to create one data set
 dataSubject <- rbind(dataSubjectTrain, dataSubjectTest)
 dataActivity<- rbind(dataActivityTrain, dataActivityTest)
@@ -32,6 +37,7 @@ dataFeaturesNames <- read.table(file.path(path_rf, "features.txt"),head=FALSE)
 names(dataFeatures)<- dataFeaturesNames$V2
 dataCombine <- cbind(dataSubject, dataActivity)
 Data <- cbind(dataFeatures, dataCombine)
+
 ### Extracting only the measurements on the mean and standard deviation for each measurement
 subdataFeaturesNames<-dataFeaturesNames$V2[grep("mean\\(\\)|std\\(\\)", dataFeaturesNames$V2)]
 selectedNames<-c(as.character(subdataFeaturesNames), "subject", "activity" )
@@ -39,6 +45,7 @@ Data<-subset(Data,select=selectedNames)
 str(Data)
 activityLabels <- read.table(file.path(path_rf, "activity_labels.txt"),header = FALSE)
 head(Data$activity,30)
+
 ### Labeling the Data set with desriptive name
 names(Data)<-gsub("^t", "time", names(Data))
 names(Data)<-gsub("^f", "frequency", names(Data))
@@ -47,6 +54,7 @@ names(Data)<-gsub("Gyro", "Gyroscope", names(Data))
 names(Data)<-gsub("Mag", "Magnitude", names(Data))
 names(Data)<-gsub("BodyBody", "Body", names(Data))
 names(Data)
+
 ### Seperate Data Set that is Tidy
 FinalData <- Data
 FinalData <- Data %>%
